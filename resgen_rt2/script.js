4808ed770531a0b265c88067c240437f92c972d6
bambook919 <78874927+bambook919@users.noreply.github.com>
2021-09-15 20:57:20 +0300

    1

--- resgen_rt2/script.js
+++
@@ -1,9 +0,0 @@
-const data = JSON.parse(localStorage.rpg2_data);
-
-data.user.balance = 999999999;
-data.user.diamonds = 999999999;
-data.user.exp += 500000;
-data.user.damage += 20000;
-
-localStorage.rpg2_data = JSON.stringify(data);
-location.href = '../'
