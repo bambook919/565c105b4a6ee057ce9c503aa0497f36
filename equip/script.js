c40996673f67d9004da86c5a47be751516250d9d
bambook919 <78874927+bambook919@users.noreply.github.com>
2021-09-16 07:16:35 +0300

    Update script.js

--- equip/script.js
+++ equip/script.js
@@ -213,7 +213,7 @@ function loadClothes() {
 
 
     el.options[data.user.clothes.helmet].selected = 'selected';
-    el1.options[data.user.clothes.armor].selected = 'selected';
+    el1.options[data.user.clothes.armor - 1].selected = 'selected';
     el2.options[data.user.clothes.pants].selected = 'selected';
 
 
