2248b3cc763a4d34e1be7ad2b6c9449d7bd3fbdc
bambook919 <78874927+bambook919@users.noreply.github.com>
2021-09-16 09:36:01 +0300

    Update script.js

--- shop/equipment/script.js
+++ shop/equipment/script.js
@@ -296,3 +296,7 @@ function buyPants(id) {
         return alert(`Недостаточно средств.`);
     }
 }
+
+data.user.clothes.boughtHelmets = data.user.clothes.boughtHelmets.sort((a, b) => a - b);
+data.user.clothes.boughtArmours = data.user.clothes.boughtArmours.sort((a, b) => a - b);
+data.user.clothes.boughtPants = data.user.clothes.boughtPants.sort((a, b) => a - b);
